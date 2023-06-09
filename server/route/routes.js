06563f37ad861435ed23e4c0fc69e04215d73cb0
Dat Ngo <ngodat02092001@gmail.com>
2023-06-10 01:24:43 +0700

    "update routes"

--- server/route/routes.js
+++ server/route/routes.js
@@ -3,10 +3,6 @@ const task = require('../controller/task.js');
  
 const router = express.Router();
  
-router.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
-
 router.get('/get-all', task.getTasks);
 router.post('/create', task.createTask);
 router.put('/edit/:id', task.editTask);
