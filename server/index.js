ef6e2a86bf1f4fa8c529d2d94a941182f5dbcbe9
Dat Ngo <ngodat02092001@gmail.com>
2023-06-18 18:04:48 +0700

    "config db & upload Dockerfile"

--- server/index.js
+++ server/index.js
@@ -8,11 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(cors());
+
 
 
 app.use('/api/task', routes);
 app.use(cors())
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.listen(port, (err) => {
+  if (err) console.log("error");
+  else console.log(`server listening on ${port}`);
 });
